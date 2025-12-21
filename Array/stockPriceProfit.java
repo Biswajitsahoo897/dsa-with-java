b1358bef05be424be227bfbd878710e981d945b0
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2025-12-21 09:47:47 +0530

    Refactor stockPriceProfit class to include maxProfit method using Math.min and max

--- Array/stockPriceProfit.java
+++ Array/stockPriceProfit.java
@@ -1,9 +1,21 @@
 package Array;
 
 public class stockPriceProfit {
-    
+    // same logic using Math.min and max (METHOD -1)
+    static int maxProfit(int[] prices) {
+        int n=prices.length;
+        int maxProfit=0;
+        int minValue=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            minValue=Math.min(prices[i],minValue);
+            maxProfit=Math.max(prices[i]-minValue,maxProfit);
+        }
+        return maxProfit;
+    }
 
     public static void main(String[] args) {
+
+        // same logic using if-else (METHOD -2)
         int prices[]={7,1,5,3,6,4};
         int n=prices.length;
         int minPrice=Integer.MAX_VALUE;
@@ -16,6 +28,7 @@ public class stockPriceProfit {
                 maxProfit=prices[i]-minPrice;
             }
         }
-        System.out.println("Maximum profit is :"+maxProfit);
+        System.out.println("Maximum profit is(using if-else) :"+maxProfit);
+        System.out.println("Maximum profit is(using Math.min and max) :"+maxProfit(prices));
     }
 }
