3f87fc5f00c6ab6e0070c2df695202882fe347e4
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2026-01-04 22:18:56 +0530

    add bit manipulation operations for integer

---
+++ Bit_manipulation/bitOperationOnInteger.java
@@ -0,0 +1,21 @@
+package Bit_manipulation;
+
+public class bitOperationOnInteger {
+    static int getIthBit(int n, int i){
+        int bitMask=1<<i;
+        return (n&bitMask)==0?0:1;
+    }
+    static int setIthBit(int n,int i){
+        int bitMask=1<<i;
+        return n|bitMask;
+    }
+    static int clearIthBit(int n,int i){
+        int bitMask=~(1<<i);
+        return bitMask&n;
+    }
+    public static void main(String[] args) {
+        System.out.println(getIthBit(10, 2));
+        System.out.println(setIthBit(10, 2));
+        System.out.println(clearIthBit(10, 1));
+    }
+}
