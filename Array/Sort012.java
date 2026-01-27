98ca8438988c8dd76b69c1764bf268672f458025
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2026-01-27 21:27:43 +0530

    initial commit , sort 0s 1s and 2s in array

---
+++ Array/Sort012.java
@@ -0,0 +1,34 @@
+package Array;
+
+
+
+public class Sort012 {
+    static void sort(int[] arr) {
+        int low = 0, mid = 0, high = arr.length - 1;
+
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else {
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 0, 1, 2};
+        sort(arr);
+
+        for (int x : arr)
+            System.out.print(x + " ");
+    }
+}
