530b01584f0093e33964a485b48ae557c3ef23f2
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2026-01-30 00:39:22 +0530

    implement stack functionality using Java Collections

---
+++ Stack/stackUsingCollections.java
@@ -0,0 +1,22 @@
+package Stack;
+
+import java.util.Stack;
+
+public class stackUsingCollections {
+    public static void main(String[] args) {
+        Stack<Integer> s=new Stack<>();
+
+        s.push(12);
+        s.push(34);
+        s.push(15);
+        s.push(82);
+        System.out.println(s.peek());
+        s.pop();
+        s.pop();
+        System.out.println(s.peek());
+        System.out.println(s.isEmpty());
+        
+        System.out.println(s.size());
+
+    }
+}
