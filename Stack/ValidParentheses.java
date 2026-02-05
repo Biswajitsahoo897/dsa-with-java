c7250d703c5fee0f4c24a5b2a85438592379a400
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2026-02-05 13:26:27 +0530

    comment out the package

--- Stack/ValidParentheses.java
+++ Stack/ValidParentheses.java
@@ -1,14 +1,14 @@
-package Stack;
+// package Stack;
 
 import java.util.Scanner;
 import java.util.Stack;
 
 public class ValidParentheses {
-    static boolean ParenthesesChecker(String str) {
+    static boolean ParenthesesChecker(String s) {
         Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
 
-            char curr = str.charAt(i);
+            char curr = s.charAt(i);
 
             if (curr == '(' || curr == '{' || curr == '[')
                 stack.push(curr);
--- Stack/stackUsingLinkedList.java
+++ Stack/stackUsingLinkedList.java
@@ -1,4 +1,4 @@
-package Stack;
+// package Stack;
 
 class Node{
     int data;
