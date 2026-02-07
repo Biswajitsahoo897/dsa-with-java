75fd569889bf5b368f41135b8b82153785bd721b
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2026-02-07 17:49:50 +0530

    Initial commit , queue using collection

---
+++ queue/queueUsingCollection.java
@@ -0,0 +1,15 @@
+package queue;
+import java.util.LinkedList;
+import java.util.Queue;
+public class queueUsingCollection {
+    public static void main(String[] args) {
+        Queue<Integer> q=new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        System.out.println(q);
+        q.remove();
+        System.out.println(q);
+    }
+}
