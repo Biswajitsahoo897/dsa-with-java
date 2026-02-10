9172df8d74c6855f55371a6bb40db5d6a71e8b63
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2026-02-10 14:16:37 +0530

    Refactor the code for only one solution & add comments

--- backtracking/N_QueenProblem.java
+++ backtracking/N_QueenProblem.java
@@ -26,20 +26,23 @@ public class N_QueenProblem {
         return true;
     }
 
-    public static void nQueen(char board[][],int row){
+    public static boolean nQueen(char board[][],int row){
         if(row==board.length){
             printBoard(board);
-            return;
+            return true;
             // u can track of the number of solution by adding one variable & increment it
         }
         for (int j = 0; j < board.length; j++) {  
             if(isSafe(board, row,j)){
                 board[row][j]='Q';
-                nQueen(board, row+1);
+                if(nQueen(board, row+1)){
+                    return true;
+                }
                 board[row][j]='X'; //backtracking step - means unplaced the previously explored ,so that i can explore other probable solutions
-                
+                // this above line executes only when recursion returns false otherwise true;
             }
         }
+        return false;
     }
     
     public static void printBoard(char b[][]){
