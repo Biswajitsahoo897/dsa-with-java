9a736e3c5ff1cfa92d5a11a571a293c3c15eef5f
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2026-03-04 01:19:50 +0530

    added the swap function

--- Sorting/bubbleSorting.java
+++ Sorting/bubbleSorting.java
@@ -1,13 +1,18 @@
 package Sorting;
 public class bubbleSorting {
+    // u can't just pass only i , j to swap function because it doesn't know which array to swap in
+    //  so we have to pass the array as well , basic coding stuff
+    static void swap(int []arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
     static void bubblesorting(int[] arr){
         int n=arr.length;
         for (int i = n-1; i >0; i--) {
             for (int j = 0; j<i; j++) {
                 if(arr[j]>arr[j+1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+                    swap(arr, j, j+1);
                 }
             }
         }
