a51676764c932ce9df6a468b37530e2c53150167
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2026-03-07 16:09:40 +0530

    add sum of pairs

---
+++ Sorting/sumOfPairs.java
@@ -0,0 +1,42 @@
+package Sorting;
+import java.util.*;
+
+public class sumOfPairs {
+    static boolean sumOfPairs(int []arr,int k){
+        int p1=0,p2=arr.length-1;
+
+        while(p1<p2){
+            if(arr[p1]+arr[p2]==k){
+                return true;
+            }else if(arr[p1]+arr[p2]<k){
+                p1++;
+            }else{
+                p2--;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+
+        while(t-->0){
+            int n=sc.nextInt();
+            int k=sc.nextInt();
+            int []arr=new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+            }
+            Arrays.sort(arr);
+            if(sumOfPairs(arr,k)){
+                System.out.println("True");
+            }else{
+                System.out.println("False");
+            }
+
+        }
+        sc.close();
+        
+    }
+}
+
