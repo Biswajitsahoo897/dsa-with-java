017d76dbd8fb1501fa00009bb9739caef3ee46f4
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2026-03-25 00:53:47 +0530

    add contains any self loop or not

---
+++ linkedlist/hasCycle.java
@@ -0,0 +1,36 @@
+package linkedlist;
+
+import java.util.HashSet;
+
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int val){
+        this.val=val;
+        this.next=null;
+    }
+}
+public class hasCycle {
+    // this method is known as floyd’s cycle detection algo
+    public boolean hasCycleF1(ListNode head) {
+        ListNode slow = head , fast = head ;
+        while(fast != null && fast.next != null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+    // what's the first thing that comes into mind is hashSet ...
+    public boolean hasCycleF2(ListNode head) {
+        HashSet<ListNode> set=new HashSet<>();
+        while(head != null){
+            if(set.contains(head)){
+                return true;
+            }
+            set.add(head);
+            head=head.next;
+        }
+        return false;
+    }
+}
