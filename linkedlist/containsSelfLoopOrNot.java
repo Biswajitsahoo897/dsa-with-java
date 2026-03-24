07b13c5db191ea6fac8472b8ae813e4f995d13d6
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2026-03-25 00:53:22 +0530

    add ListNode and contains Self Loop Or Not

---
+++ linkedlist/containsSelfLoopOrNot.java
@@ -0,0 +1,12 @@
+package linkedlist;
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int val){
+        this.val=val;
+        this.next=null;
+    }
+}
+public class containsSelfLoopOrNot {
+    
+}
