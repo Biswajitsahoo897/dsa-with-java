6f2487803ae9e7d4a6fb82cd993581910978e05c
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2026-04-03 13:28:36 +0530

    add stdRankSorting implementation using PriorityQueue

---
+++ heaps/stdRankSorting.java
@@ -0,0 +1,33 @@
+package heaps;
+
+import java.util.PriorityQueue;
+
+public class stdRankSorting {
+    static class Student implements Comparable<Student>{
+        String name;
+        int rank;
+
+        public Student(String name,int rank){
+            this.name=name;
+            this.rank=rank;
+        }
+
+        @Override
+        public int compareTo(Student s2){
+            return this.rank-s2.rank;
+        }
+    }
+    public static void main(String[] args) {
+        PriorityQueue<Student> pq=new PriorityQueue<>();
+
+        pq.add(new Student("A", 4)); //O(logn)  insertion, deletion ->O(1) 
+        pq.add(new Student("D", 1));
+        pq.add(new Student("B", 3));
+        pq.add(new Student("C", 2));
+
+        while (!pq.isEmpty()) {
+            System.out.println(pq.peek().name+" "+pq.peek().rank);
+            pq.remove();
+        }
+    }
+}
