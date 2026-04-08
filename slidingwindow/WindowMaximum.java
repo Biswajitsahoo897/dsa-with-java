83bd2325f6e585c99a778339fcdcbcb5f4c6ee13
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2026-04-08 09:01:33 +0530

    add max element sum of the window

---
+++ slidingwindow/WindowMaximum.java
@@ -0,0 +1,51 @@
+package slidingwindow;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import java.io.*;
+public class WindowMaximum {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));        
+        BufferedWriter wr=new BufferedWriter(new OutputStreamWriter(System.out));        
+        int t=Integer.parseInt(br.readLine());
+        while(t-->0){
+            int n,k;
+            String []input=br.readLine().split(" ");
+            n=Integer.parseInt(input[0]);
+            k=Integer.parseInt(input[1]);
+
+            
+            String []inp=br.readLine().split(" ");
+            int []arr=new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=Integer.parseInt(inp[i]);
+            }
+
+            Deque<Integer> d=new ArrayDeque<>();
+            long sum=0;
+            for(int i=0;i<n;i++){
+                // remove from the window
+                if(!d.isEmpty() && d.peekFirst()==i-k){
+                    d.pollFirst();
+                }
+                // dec order(if the curr element is greater than previous one then that is of no use so gotta remove them)
+
+                while(!d.isEmpty() && arr[d.peekLast()]<=arr[i]){
+                    d.pollLast();
+                }
+
+                d.offerLast(i);
+
+                if(i>=k-1){
+                    sum+=arr[d.peekFirst()];
+                }
+            }
+            wr.write(sum+"\n");
+        }
+        wr.flush();
+        
+    }
+}
+
