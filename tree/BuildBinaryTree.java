764ca1bf32a0a8080a96f2cf961395776f675d0a
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2026-04-22 13:54:46 +0530

    add level order traversal in BT

--- tree/BuildBinaryTree.java
+++ tree/BuildBinaryTree.java
@@ -1,5 +1,8 @@
 package tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BuildBinaryTree {
     static class Node {
         int data;
@@ -10,6 +13,7 @@ public class BuildBinaryTree {
             this.data=data;
         }
     }
+    static Node root=null;
     static int idx=-1;
     static Node buildTree(int []nodes){
         idx++;
@@ -21,7 +25,7 @@ public class BuildBinaryTree {
 
         return newNode;
     }
-
+    // left , root , right
     static void inorderTraversal(Node root){
         if(root==null) return;
 
@@ -46,13 +50,33 @@ public class BuildBinaryTree {
         System.out.print(root.data+" ");
     }
 
+    static void levelOrderTraversalBT(){
+        // as we are maintaining a queue so the value will only remove by left Node and right Node(order)
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        
+        while(!q.isEmpty()){
+            int size=q.size();
+
+            for(int i=0;i<size;i++){
+                Node curr=q.poll();
+                System.out.print(curr.data+" ");
+                if(curr.left!=null) q.add(curr.left);
+                if(curr.right!=null) q.add(curr.right);
+            }
+            System.out.println();
+        }
+
+    }
     public static void main(String[] args) {
         int []nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
-        Node root=buildTree(nodes);
+        root=buildTree(nodes);
 
         System.out.println(root.left);
         postorderTraversal(root);
         System.out.println();
+
+        levelOrderTraversalBT();
 
     }
 }
