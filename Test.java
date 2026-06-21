e9b626a94f07caa3cd244f4b590f2f15787215f2
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2026-06-21 22:04:14 +0530

    made some changes

--- Test.java
+++ Test.java
@@ -16,11 +16,32 @@ public class Test {
                 //     xor ^= i;
                 // }
                 // System.out.println(10|1);
-        int []arr={1,5,7,2,4,5,6};
-        System.out.println(Arrays.stream(arr).max().getAsInt());
-        System.out.println(Arrays.stream(arr).min().getAsInt());
-        System.out.println(Arrays.stream(arr).sum());
-
+        // int []arr={1,5,7,2,4,5,6};
+        // System.out.println(Arrays.stream(arr).max().getAsInt());
+        // System.out.println(Arrays.stream(arr).min().getAsInt());
+        // System.out.println(Arrays.stream(arr).sum());
+        // StringBuilder sb=new StringBuilder();
+        // String s="acbzq";
+        // // sb.append("234123");
+        // for(int i=0;i<s.length();i++){
+        //     int currNum=s.charAt(i)-'a'+1;
+        //     sb.append(currNum);
+        // }
+        // int num=Integer.parseInt(sb.toString());
+        // System.out.println(num);
+        // System.out.println(sb);
+        String s="the book is right the ";
+        s=s.trim();
+        int len=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)!=' '){
+                len++;
+            }
+            else{
+                break;
+            }
+        }
+        System.out.println(len);
     }
 }
 
--- Test2.java
+++ Test2.java
@@ -1,22 +1,56 @@
 import java.util.*;
+
 public class Test2 {
+    // static int a=89;
 
-    static void subsets(String s, int index, String ans){
+    // static void subsets(String s, int index, String ans){
 
-        if(index == s.length()){
-            if(ans.length() > 0)
-                System.out.println(ans);
-            return;
-        }
-        // include
-        subsets(s, index+1, ans + s.charAt(index));
+    //     if(index == s.length()){
+    //         if(ans.length() > 0)
+    //             System.out.println(ans);
+    //         return;
+    //     }
+    //     // include
+    //     subsets(s, index+1, ans + s.charAt(index));
 
-        // exclude
-        subsets(s, index+1, ans);
+    //     // exclude
+    //     subsets(s, index+1, ans);
+    // }
+    static void change(int []arr){
+        arr[1]=99;
     }
-
     public static void main(String[] args){
-        subsets("ABC",0,"");
+        // subsets("ABC",0,"");
+        // int a =90;
+        // System.out.println(a);
+        // change(12);
+        // System.out.println(a);   
+
+        // int []arr={1,4,6};
+        // change(arr);
+        // for(int x:arr){
+        //     System.out.print(x+" ");
+        // }
+        String num="52192";
+        int n=Integer.parseInt(num);
+        int original=n;
+        String res="";
+        int largestOdd=-1;
+        for(int i=0;i<num.length();i++){
+            while(n!=0){
+                int rem=n%10;
+                if(rem%2==1){
+                    largestOdd=Math.max(largestOdd,rem);
+                }
+                res+=largestOdd;
+                n/=10;
+            }
+        }
+        System.out.println(res);
+
+        String s="aSd";
+
+        System.out.println((char)((int)s.charAt(1)+32));
     }
 }
 
